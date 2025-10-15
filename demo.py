ef6151a646b38a6a0a44e9749567e41425558c3f
Rashed Aldulijan <rashdul@hotmail.com>
2025-10-14 21:43:41 -0400

    Final

--- demo.py
+++ demo.py
@@ -1,10 +1,3 @@
-import numpy as np
+import torch
 
-# build big matrix
-A = np.random.rand(20, 20)
-
-print(f'A: {A}')
-
-sample = np.random.choice(A.shape[0], size=5, replace=True)
-
-print(f'sample: {A[np.random.choice(A.shape[0], size=2, replace=True), :]}')
+print(torch.zeros((5, 5)))
